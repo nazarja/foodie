19a086f5f37b44a9724035b5c20b6abd09a67488
Sean Murphy <mail.seanmurphy@gmail.com>
2019-03-01 10:20:25 +0000

    Development: Added Filters Schema

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,13 @@ from app import app
 from flask import render_template
 
 
+@app.context_processor
+def inject_filters():
+    with open('app/data/schemas/filters.json') as filters_file:
+        filters = json.load(filters_file)
+        return dict(filters=filters)
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
