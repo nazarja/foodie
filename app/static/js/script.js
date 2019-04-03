1a3ee65e1f868fb6378275505dc6053c242a477f
Sean Murphy <mail.seanmurphy@gmail.com>
2019-04-03 15:05:41 +0100

    Development: Added JavaScript Logic to allow user to like / dislike a recipe.

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -65,7 +65,8 @@ function listenForComments() {
                     `<article class="uk-comment uk-padding-small uk-margin-medium"><header class="uk-comment-header uk-grid-medium uk-flex-middle" uk-grid>
                     <div class="uk-width-auto"><img class="uk-comment-avatar" src="../../static/images/profile_picture.png" width="80" height="80" alt="profile picture"></div>
                     <div class="uk-width-expand"><h4 class="uk-comment-title uk-margin-remove">${data.value}</h4><div class="uk-comment-meta">
-                    <a href="{{ url_for('recipe', recipe=${recipeId}, title=comment.title|resub)}}">${recipeTitle}</a></div><ul class="uk-comment-meta uk-subnav uk-subnav-divider uk-margin-remove-top">
+                    <a href="{{ url_for('recipe', recipe=${recipeId}, title=comment.title|resub)}}">${recipeTitle}</a>
+                    </div><ul class="uk-comment-meta uk-subnav uk-subnav-divider uk-margin-remove-top">
                     <li>${date}</li></ul></div></header><div class="uk-comment-body"><p class="comment">${reply.value}</p></div></article>`;
                     
                     if (placeholder) placeholder.style.display = 'none';
@@ -79,3 +80,70 @@ function listenForComments() {
     }
 }
 listenForComments();
+
+
+/*
+==================================================================
+   Like / Dislike XHR Function
+==================================================================
+*/
+
+function listenForFavourites() {
+
+   // if the user is not logged in - do not continue 
+   if (userLoggedIn == 'false') return promptToLogin();
+
+   const favourites = document.querySelectorAll('.favourites');
+   favourites.forEach(favourite => favourite.addEventListener('click', event => {
+        let [_id, opinion, value] = event.currentTarget.dataset.values.split(',');
+
+        favourites.forEach(item => {
+            let [itemId, itemOpinion, itemValue] = item.dataset.values.split(',');
+            let text = item.childNodes[0];
+            let num = parseInt(text.textContent);
+            
+            // if we have the correct recipe
+            if (_id == itemId) {
+
+                // if opinion is the same 
+                if (itemOpinion == opinion) {
+                       
+                        // if were are removing a opinion
+                        if (itemValue == 'true') {
+                            item.classList.remove('user-favourite');
+                            item.dataset.values = `${itemId},${itemOpinion},false`;
+                            text.textContent = `${num - 1} `;
+                        }
+                        // if we are adding an opinion
+                        else {
+                            item.classList.add('user-favourite');
+                            item.dataset.values = `${itemId},${itemOpinion},true`;
+                            text.textContent = `${num + 1} `;
+                        }
+                }
+
+                // you cannot like and dislike a recipe at the same time
+                // remove the opossing opinion
+                if (itemOpinion != opinion) {
+                    if (itemValue == 'true') {
+                        item.classList.remove('user-favourite');
+                        item.dataset.values = `${itemId},${itemOpinion},false`;
+                        text.textContent = `${num - 1} `;
+                    }
+                }
+            };
+        });
+   }));
+};
+listenForFavourites();
+
+
+/*
+==================================================================
+   Login Prompt
+==================================================================
+*/
+
+function promptToLogin() {
+    console.log('You need to login to do that!');
+}
